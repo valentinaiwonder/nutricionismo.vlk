c412b878f41676bab138a6e7ce3ce55376847e87
KenzoMitsu <167575457+KenzoMitsu@users.noreply.github.com>
2024-05-10 10:15:52 -0300

    Alterações Calculadora

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -13,20 +13,21 @@ def contato():
 
 @app.route('/servico')
 def servico():
-    return reder_template('servico.html')
+    return render_template('servico.html')
 
 
 @app.route('/calculadora')
 def imc():
-    return reder_template('calculadora.html')
+    return render_template('calculadora.html')
 
 
 @app.route('/resultado', methods=['POST'])
-def calcular_imc():
-    peso = float(input("Digite seu peso em kg: "))
-    altura = float(input("Digite sua altura em metros: "))
-    imc = peso / (altura ** 2)
-    print("Seu IMC é: {:.2f}".format(imc))
+def resultado():
+    peso = float(request.form["peso"])
+    altura = float(request.form["altura"])
+
+    imc = round(peso / (altura**2), 2)
+    return render_template('calculadora.html', resultado=imc)
 
 
 if __name__ == '__main__':
